ec67d3d1e4aa80dbb99ca852649e50d9b078c892
SrEM89 <ruangsee2601@gmail.com>
2023-09-19 16:08:34 +0700

    init

---
+++ pages/demo.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="my-5">
+   <h1 class="my-5">Demo Page</h1>
+   <p class="my-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id quam, rem voluptate error minus adipisci! Inventore explicabo dolor laudantium architecto?</p>
+  </div>
+</template>
+
+<style scoped></style>
